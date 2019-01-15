cf2cd8ce2a4ef3720a2c97e98c32def45d0f4517
IldusTim <ildustimerkhanov@gmail.com>
2019-01-16 03:00:42 +0500

    lowwww

--- src/main/java/com/easyqa/qa/pages/IssuesPage.java
+++ src/main/java/com/easyqa/qa/pages/IssuesPage.java
@@ -37,7 +37,7 @@ public class IssuesPage {
     @FindBy(xpath = "//label[.='Medium']")
     public SelenideElement issuePriorityMedium;
 
-    @FindBy(xpath = "//label[.='Low']")
+    @FindBy(xpath = "//label[.='Lowwww']")
     public SelenideElement issuePriorityLow;
 
     @FindBy(name = "commit")
